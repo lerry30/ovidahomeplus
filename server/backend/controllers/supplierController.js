d9bea52b9293d83be7a99e9e06a28ef95db22376
Lerry <johnlerryramossamson@gmail.com>
2024-11-06 15:42:34 +0800

    Controller for supplier added

--- server/backend/controllers/supplierController.js
+++ server/backend/controllers/supplierController.js
@@ -1,4 +1,5 @@
 import { requestHandler } from '../utils/requestHandler.js';
+import * as supplierStmt from '../mysql/supplier.js';
 
 /*
    desc     New Supplier
@@ -8,10 +9,16 @@ import { requestHandler } from '../utils/requestHandler.js';
 const newSupplier = requestHandler(async (req, res, database) => {
     const name = String(req.body?.name).trim();
     const contact = String(req.body?.contact).trim();
-    const image = req.body?.image;
+    const image = req?.file?.filename || '';
 
     if(!name) throw {status: 400, message: 'Supplier name is required to add new supplier.'}
-    console.log(image.size);
+
+    const [insert] = await database.execute(supplierStmt.newSupplier, [name, contact, image]);
+    if(insert?.insertId > 0) {
+        res.status(201).json({message: 'Inserted successfully.'});
+    } else {
+        throw {status: 401, message: 'New supplier failed to insert.'};
+    }
 });
 
 export {
