b6c484a3eeaf5ec78821498aff42e37c4238efd3
Lerry <johnlerryramossamson@gmail.com>
2024-11-06 15:41:09 +0800

    Config for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,5 +11,11 @@ export default defineConfig({
   },
   server: {
     port: 3000
-  }
+  },
+  proxy: {
+    '/api': {
+      target: 'http://localhost:4000',
+      secure: false
+    },
+  },
 });
