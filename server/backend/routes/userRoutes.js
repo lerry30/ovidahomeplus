7ee672193beeabd84f1dc4638bc1d07340759a9d
Lerry <johnlerryramossamson@gmail.com>
2024-11-06 15:47:20 +0800

    User route added for authenticating user credentials

--- server/backend/routes/userRoutes.js
+++ server/backend/routes/userRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
 
 import { 
     authUser, 
-    registerUser, 
+    registerUser,
+    getUser,
 } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
+router.get('/user', protect, getUser);
 
 export default router;
