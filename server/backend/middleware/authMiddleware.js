7be8d01e921ecdf7de4591553dadb0447c0ee3ec
Lerry <johnlerryramossamson@gmail.com>
2024-11-06 15:44:59 +0800

    Fixed middleware

--- server/backend/middleware/authMiddleware.js
+++ server/backend/middleware/authMiddleware.js
@@ -1,17 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-import * as employeeStmt from '../mysql/statements.js';
+import * as employeeStmt from '../mysql/employee.js';
 import { requestHandler } from '../utils/requestHandler.js';
 
 const protect = requestHandler(async (req, res, database, next) => {
-    console.log(req.cookies);
     const token = req.cookies.jwt;
 
     if(token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             const [id, username] = decoded.value.split('-');
-            const [rows] = await database.execute(employeeStmt.employee, [username]);
+            const [rows] = await database.execute(employeeStmt.username, [username]);
             if(rows.length > 0) {
                 req.user = rows[0];
                 next();
@@ -20,6 +19,7 @@ const protect = requestHandler(async (req, res, database, next) => {
                 throw new Error('User not found');
             }
         } catch(error) {
+            console.log(error);
             res.status(401);
             throw new Error('Not authorized, invalid token');
         }
