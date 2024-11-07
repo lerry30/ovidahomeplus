01f91191739d3b9f0b44ffc29c7287173ce356fa
Lerry <johnlerryramossamson@gmail.com>
2024-11-07 17:00:43 +0800

    Add controllers

--- server/backend/controllers/supplierController.js
+++ server/backend/controllers/supplierController.js
@@ -1,4 +1,5 @@
 import { requestHandler } from '../utils/requestHandler.js';
+import { toNumber } from '../utils/number.js';
 import * as supplierStmt from '../mysql/supplier.js';
 
 /*
@@ -26,7 +27,42 @@ const getSuppliers = requestHandler(async (req, res, database) => {
     res.status(200).json({result});
 });
 
+const changeSupplierStatus = requestHandler(async (req, res, database) => {
+    const id = toNumber(req.body?.id);
+    const changeTo = String(req.body?.changeTo).toLowerCase().trim();
+
+    if(!id) throw {status: 404, message: 'Supplier not recognized.'};
+    if(changeTo !== 'active' && changeTo !== 'inactive') throw {status: 400, message: 'Error status type of undefined.'};
+
+    const [result] = await database.execute(supplierStmt.changeStatus, [changeTo, id]);
+    if(result?.changedRows > 0) {
+        res.status(200).json({message: 'Supplier status updated.'});
+    } else {
+        throw {status: 400, message: 'Updating supplier status failed.'}
+    }
+});
+
+const updateSupplier = requestHandler(async (req, res, database) => {
+    const id = toNumber(req.body?.id);
+    console.log(req.body, id);
+    const name = String(req.body?.name)?.trim();
+    const contact = String(req.body?.contact).trim();
+    const image = req?.file?.filename || '';
+
+    if(!id || id===0) throw new Error('There\'s something wrong.');
+    if(!name) throw {status: 400, message: 'Supplier name is required to update supplier.'}
+
+    const [update] = await database.execute(supplierStmt.updateSupplier, [name, contact, image, id]);
+    if(update?.changedRows > 0) {
+        res.status(200).json({message: 'Supplier successfully updated.'});
+    } else {
+        throw {status: 400, message: 'Updating supplier failed.'}
+    }
+});
+
 export {
     newSupplier,
     getSuppliers,
+    changeSupplierStatus,
+    updateSupplier,
 };
