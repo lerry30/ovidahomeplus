383af9b86ae5755530ce6b619b011c7cdadbf6ae
Lerry <johnlerryramossamson@gmail.com>
2024-11-07 17:06:50 +0800

    New store added

---
+++ frontend/src/store/supplier.js
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { supplier as localStorageName } from '@/constants/localStorageNames';
+
+export const zSupplier = create(set => ({
+    id: 0,
+    name: '',
+    contact: '',
+    image: '',
+    status: '',
+
+    saveSupplierData: (id, name, contact, image, status) => {
+        localStorage.setItem(localStorageName, JSON.stringify({id, name, contact, image, status}));
+        set(state => ({id, name, contact, image, status}));
+    },
+
+    reloadSupplierData: () => {
+        const localSupplierData = JSON.parse(localStorage.getItem(localStorageName) || '{}');
+        if(Object.keys(localSupplierData)?.length > 0) {
+            const {id, name, contact, image, status} = localSupplierData;
+            set(state => ({id, name, contact, image, status}));
+        }
+    }
+}));
