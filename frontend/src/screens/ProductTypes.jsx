fbf2d7f9ab9ee5cb9a99455cc8e4754dea44f8e0
Lerry <johnlerryramossamson@gmail.com>
2024-11-12 17:36:16 +0800

    Page modified

--- frontend/src/screens/ProductTypes.jsx
+++ frontend/src/screens/ProductTypes.jsx
@@ -179,7 +179,17 @@ const ProductTypes = () => {
             </section>
             <h1 className="flex sm:hidden font-semibold text-lg">Product Types</h1>
             <div className="grow w-full h-full relative">
-                <div className="w-full absolute top-0 left-0 right-0 bottom-0 bg-white mt-2 rounded-lg shadow-md overflow-hidden">
+                {/* container with scroll bar */}
+                <div className="w-full absolute top-0 left-0 right-0 bottom-0 bg-white mt-2 rounded-lg shadow-md 
+                    overflow-x-hidden overflow-y-auto
+                    [&::-webkit-scrollbar]:w-2
+                    [&::-webkit-scrollbar-track]:rounded-full
+                    [&::-webkit-scrollbar-track]:bg-gray-100
+                    [&::-webkit-scrollbar-thumb]:rounded-full
+                    [&::-webkit-scrollbar-thumb]:bg-gray-300
+                    dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+                    dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500
+                ">
                     <div className="h-[40px] border-b p-2 flex gap-2">
                         <button onClick={() => tabNavigate('all')} className={`rounded-lg px-2 ${tabs.all&&'bg-green-600 text-white'}`}>All</button>
                         <button onClick={() => tabNavigate('active')} className={`rounded-lg px-2 ${tabs.active&&'bg-green-600 text-white'}`}>Active</button>
