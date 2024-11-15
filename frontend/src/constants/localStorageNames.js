5ac4f1783fee7ad256b2848a926a61511895c390
Lerry <johnlerryramossamson@gmail.com>
2024-11-15 17:55:51 +0800

    New storage name added

--- frontend/src/constants/localStorageNames.js
+++ frontend/src/constants/localStorageNames.js
@@ -2,10 +2,12 @@ const user = 'user-data';
 const supplier = 'supplier-data';
 const productType = 'product-type';
 const item = 'item-data';
+const selectedItems = 'selected-items-data';
 
 export { 
    user,
    supplier,
    productType,
    item,
+   selectedItems,
 };
