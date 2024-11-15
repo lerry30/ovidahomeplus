9e44155e3924a0af3f16378ecf56df592b2f705b
Lerry <johnlerryramossamson@gmail.com>
2024-11-15 17:56:37 +0800

    New tab added

--- frontend/src/routes/SidebarRoute.jsx
+++ frontend/src/routes/SidebarRoute.jsx
@@ -5,12 +5,14 @@ import Dashboard from '@/screens/Dashboard';
 import Inventory from '@/screens/Inventory';
 import Supplier from '@/screens/Supplier';
 import ProductTypes from '@/screens/ProductTypes';
+import Cashier from '@/screens/Cashier';
 
 const SidebarRoute = () => {
     return (
         <Routes>
             <Route element={<Sidebar />}>
                 <Route index element={<Dashboard />} />
+                <Route path="/cashier" element={<Cashier />}/>
                 <Route path="/inventory" element={<Inventory />}/>
                 <Route path="/product-types" element={<ProductTypes />} />
                 <Route path="/supplier" element={<Supplier />}/>
