2c3e4a11bfbcf8e857ff6226920fc7777a96bd02
Lerry <johnlerryramossamson@gmail.com>
2024-11-27 15:22:25 +0800

    Barcode statement updated

--- server/backend/mysql/barcode.js
+++ server/backend/mysql/barcode.js
@@ -1,4 +1,3 @@
 
 
-
-export const newBarcode = 'INSERT INTO barcodes(item_id, batch_id, barcode) VALUES(?, ?, ?);';
+export const newBarcode = 'INSERT INTO barcodes(item_id, batch_no, barcode) VALUES'; // this statement will be use for multi insert of items so controller will be in charge of setting it
