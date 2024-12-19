918cfd9d183aa97a6b7af2bbb631ec7854dd6b08
lerry <johnlerryramossamson@gmail.com>
2024-12-19 11:35:16 +0800

    Update utils

--- frontend/src/utils/signout.js
+++ frontend/src/utils/signout.js
@@ -5,7 +5,10 @@ import * as localStorageNames from '@/constants/localStorageNames';
 export const signout = async () => {
     zUser.getState().wipeOutData();
     
-    indexedDB.deleteDatabase('ovidaIDb');
+    for(const key in localStorageNames) {
+        const storageName = localStorageNames[key];
+        localStorage.removeItem(storageName);
+    }
 
     // const response = await fetch('/api/users/signout', { method: 'POST' });
     // return !!response?.success;
